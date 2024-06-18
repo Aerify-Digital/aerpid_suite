b62f1c15486f88d569eed2fc0ef62b1449d470f3
SeqSEE <SeqSEE@users.noreply.github.com>
2024-06-18 19:16:31 -0400

    enable rpm build

--- forge.config.ts
+++ forge.config.ts
@@ -25,7 +25,7 @@ const config: ForgeConfig = {
   makers: [
     new MakerSquirrel({}),
     new MakerZIP({}, ['darwin']),
-    //new MakerRpm({}),
+    new MakerRpm({}),
     new MakerDeb({})
   ],
   hooks: {
